924af1839d73d33a95ec3f35d6c804734bd6d334
Kefe05 <oghenekefe.okpare@miva.edu.ng>
2025-10-24 15:39:19 +0100

    image fix

--- src/app/about/component/about.tsx
+++ src/app/about/component/about.tsx
@@ -16,7 +16,7 @@ export default function AboutPage() {
             <div className="relative aspect-[4/3] rounded-3xl overflow-hidden shadow-2xl">
               {/* Background Image */}
               <Image
-                src="/https://res.cloudinary.com/dyzanjli6/image/upload/v1761076239/vwt0wvek2tbdokdoey8y.jpg"
+                src="https://res.cloudinary.com/dyzanjli6/image/upload/v1761076239/vwt0wvek2tbdokdoey8y.jpg"
                 alt="Hyper Nexium Team"
                 fill
                 className="object-cover"
